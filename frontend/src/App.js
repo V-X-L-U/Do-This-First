dc003ef6055ca3206d8221761f6a5ae9267a8779
Vallens Kho <secure.mail2357@gmail.com>
2022-05-22 16:59:22 -0400

    Sample app with test API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect } from "react";
+import { instance } from "./instance";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const testCall = async () => {
+    await instance
+      .get("/")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => {
+        console.log("[ERROR] Test call at / failed");
+      });
+  };
+
+  useEffect(() => {
+    testCall();
+  });
   return <div>Do This First</div>;
-}
+};
 
 export default App;
