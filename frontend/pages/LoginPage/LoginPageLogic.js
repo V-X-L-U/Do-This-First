6a5244c17834050e62e820ea6a644913d3d75f01
carlosds27 <carlos.saputra@mail.utoronto.ca>
2022-06-15 23:06:54 +0700

    Finish Email and Password Validation

--- frontend/pages/LoginPage/LoginPageLogic.js
+++ frontend/pages/LoginPage/LoginPageLogic.js
@@ -2,18 +2,38 @@
 // appropriate error message.
 const validateEmailandPassword = ({ email, password }) => {
   // TODO : validate email and password
+  if (validateEmail(email).localeCompare("") !== 0) {
+    return validateEmail(email);
+  } else if (validatePassword(password).localeCompare("") !== 0) {
+    return validatePassword(password);
+  } else {
+    return "";
+  }
 };
 
 // Returns an empty string if email is valid. Otherwise, return appropriate
 // error message.
 const validateEmail = ({ email }) => {
   // TODO : recommended helper
+  const emailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.[a-zA-Z0-9]{2,3})+$/;
+  if (email.match(emailFormat)) {
+    return "";
+  } else {
+    return "Error : Invalid Email Format";
+  }
 };
 
 // Returns an empty string if password is valid. Otherwise, return appropriate
 // error message.
 const validatePassword = ({ password }) => {
   // TODO : recommended helper
+  const passwordFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  // Minimum One Uppercase, One Lowercase, One Number and 8 Character (No special Character)
+  if (password.match(passwordFormat)) {
+    return "";
+  } else {
+    return "Error : Invalid Password Format";
+  }
 };
 
 // Makes a call to the server to login the user.
