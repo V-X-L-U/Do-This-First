b7ac375988dc95afa0f218f1f479747cf231a143
Vallens Kho <secure.mail2357@gmail.com>
2022-06-16 08:39:06 -0400

    Update Task Schema

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema(
     {
+        user_id: {
+            type: String,
+            required: true,
+        },
         name: {
             type: String,
             required: true,
@@ -10,15 +14,15 @@ const taskSchema = mongoose.Schema(
             type: String,
             required: true,
         },
-        prereqsDone: {
+        prereqs_done: {
             type: Boolean,
             required: true,
         },
-        taskDone: {
+        task_done: {
             type: Boolean,
             required: true,
         },
-        prerequisites: {
+        prereqs: {
             type: [String],
             required: true,
         },
