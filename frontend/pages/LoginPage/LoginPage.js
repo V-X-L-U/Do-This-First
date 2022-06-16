99c12eac0ce2dd47c7f2e2ef866e721514497f1a
carlosds27 <carlos.saputra@mail.utoronto.ca>
2022-06-16 14:20:21 +0700

    Convention Comment Fixed

--- frontend/pages/LoginPage/LoginPage.js
+++ frontend/pages/LoginPage/LoginPage.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { validateEmailandPassword } from "./LoginPageLogic";
+import {
+  validateEmailandPassword,
+  loginUserHandler,
+  registerUserHandler,
+} from "./LoginPageLogic";
 
 const LoginPage = () => {
   const [loginFormData, setLoginFormData] = { email: "", password: "" };
--- frontend/pages/LoginPage/LoginPageLogic.js
+++ frontend/pages/LoginPage/LoginPageLogic.js
@@ -2,13 +2,15 @@
 // appropriate error message.
 const validateEmailandPassword = ({ email, password }) => {
   // TODO : validate email and password
-  if (validateEmail(email).localeCompare("") !== 0) {
-    return validateEmail(email);
-  } else if (validatePassword(password).localeCompare("") !== 0) {
-    return validatePassword(password);
-  } else {
-    return "";
+  const validEmail = validateEmail(email);
+  const validPassword = validatePassword(password);
+  if (validEmail.length > 0) {
+    return validEmail;
   }
+  if (validPassword.length > 0) {
+    return validPassword;
+  }
+  return "";
 };
 
 // Returns an empty string if email is valid. Otherwise, return appropriate
@@ -16,11 +18,11 @@ const validateEmailandPassword = ({ email, password }) => {
 const validateEmail = ({ email }) => {
   // TODO : recommended helper
   const emailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.[a-zA-Z0-9]{2,3})+$/;
+  // https://www.w3resource.com/javascript/form/email-validation.php
   if (email.match(emailFormat)) {
     return "";
-  } else {
-    return "Error : Invalid Email Format";
   }
+  return "Invalid Email";
 };
 
 // Returns an empty string if password is valid. Otherwise, return appropriate
@@ -29,11 +31,11 @@ const validatePassword = ({ password }) => {
   // TODO : recommended helper
   const passwordFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
   // Minimum One Uppercase, One Lowercase, One Number and 8 Character (No special Character)
+  // https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
   if (password.match(passwordFormat)) {
     return "";
-  } else {
-    return "Error : Invalid Password Format";
   }
+  return "Invalid Password";
 };
 
 // Makes a call to the server to login the user.
