c1327022770ef86242f7811d397e6355f40c555e
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-03 18:06:31 -0400

    Add bgColor parameter to PageHeader

--- frontend/src/components/PageHeader/PageHeader.js
+++ frontend/src/components/PageHeader/PageHeader.js
@@ -1,12 +1,22 @@
 import styles from "./PageHeader.module.css";
 import PropTypes from "prop-types";
 
-const PageHeader = ({ pageTitle }) => {
-  return <h1 className={styles.PageHeader}>{pageTitle}</h1>;
+const PageHeader = ({ pageTitle, bgColor }) => {
+  return (
+    <h1
+      className={styles.PageHeader}
+      style={{
+        backgroundColor: bgColor,
+      }}
+    >
+      {pageTitle}
+    </h1>
+  );
 };
 
 PageHeader.propTypes = {
   pageTitle: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
 };
 
 export default PageHeader;
