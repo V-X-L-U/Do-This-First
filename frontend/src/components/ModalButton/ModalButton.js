f9c2ca720707655d326e88e12ce52e8a6f7bb72f
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-04 15:53:12 -0400

    Add color parameter to modalButton

--- frontend/src/components/ModalButton/ModalButton.js
+++ frontend/src/components/ModalButton/ModalButton.js
@@ -1,10 +1,14 @@
 import styles from "./ModalButton.module.css";
+import ThemeColors from "../../ThemeColors";
 import PropTypes from "prop-types";
 
-const ModalButton = ({ label, onClick, isEmphasized, color }) => {
+const ModalButton = ({ label, onClick, isEmphasized, bgColor, color }) => {
   return (
     <button
-      color={color}
+      style={{
+        backgroundColor: isEmphasized ? bgColor : ThemeColors.Grey,
+        color: isEmphasized ? color : "#c0c0c0",
+      }}
       className={isEmphasized ? styles.emphasizedButton : styles.greyedButton}
     >
       {label}
@@ -16,6 +20,7 @@ ModalButton.propTypes = {
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   isEmphasized: PropTypes.bool.isRequired,
+  bgColor: PropTypes.bool.isRequired,
   color: PropTypes.bool.isRequired,
 };
 
