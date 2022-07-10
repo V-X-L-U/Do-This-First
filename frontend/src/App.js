c963187347f1fe90bbe2be41011bae3d28c51dc1
carlosds27 <carlos.saputra@mail.utoronto.ca>
2022-07-10 17:24:51 +0700

    Add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
 import { useState } from "react";
 import { instance } from "./instance";
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationModal from "./pages/RegistrationModal/RegistrationModal";
+import TaskListPage from "./pages/TaskListPage/TaskListPage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegistrationModal />} />
+      <Route path="tasks" element={<TaskListPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 const App = () => {
   const [messageDisplay, setMessageDisplay] = useState("");
