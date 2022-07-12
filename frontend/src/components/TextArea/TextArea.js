86c1496164e6229f79213490b6d27434cefd109b
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-12 10:39:02 -0400

    Add textArea component

---
+++ frontend/src/components/TextArea/TextArea.js
@@ -0,0 +1,8 @@
+import PropTypes from "prop-types";
+import styles from "./TextArea.module.css";
+
+const TextArea = () => {
+  return <textarea className={styles.textArea} />;
+};
+
+export default TextArea;
