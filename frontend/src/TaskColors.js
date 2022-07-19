1ac19f072ac745ffca1ef5c557665c312573986d
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-19 14:29:54 -0400

    Seperate task colors to seperate file

---
+++ frontend/src/TaskColors.js
@@ -0,0 +1,7 @@
+const TaskColors = {
+  ValidTask: "#904E55",
+  InvalidTask: "#8B898B",
+  FinishedTask: "#B3B2B1",
+};
+
+export default TaskColors;
--- frontend/src/ThemeColors.js
+++ frontend/src/ThemeColors.js
@@ -3,9 +3,6 @@ const ThemeColors = {
   White: "#F2EFE9",
   Grey: "#8B898B",
   LightGrey: "#B3B2B1",
-  ValidTask: "#904E55",
-  InvalidTask: "#8B898B",
-  FinishedTask: "#B3B2B1",
 };
 
 export default ThemeColors;
