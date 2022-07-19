a3f38277beb807c06fc71c1a7a973f94d09eff95
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-19 14:33:24 -0400

    merge TaskColors into ThemeColors as a seperate object

--- frontend/src/ThemeColors.js
+++ frontend/src/ThemeColors.js
@@ -5,4 +5,10 @@ const ThemeColors = {
   LightGrey: "#B3B2B1",
 };
 
-export default ThemeColors;
+const TaskColors = {
+  ValidTask: "#904E55",
+  InvalidTask: "#8B898B",
+  FinishedTask: "#B3B2B1",
+};
+
+export { ThemeColors, TaskColors };
