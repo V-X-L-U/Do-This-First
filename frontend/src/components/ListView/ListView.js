58db88e7ee32c42d93e6455f2b1a600c938ce074
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-21 16:06:56 -0400

    Add Dynamic ListView

--- frontend/src/components/ListView/ListView.js
+++ frontend/src/components/ListView/ListView.js
@@ -1,18 +1,44 @@
 import { useState } from "react";
 import styles from "./ListView.module.css";
-import ListViewItem from "../LIstViewItem/ListViewItem";
+import PropTypes from "prop-types";
+import TaskListItem from "../TaskListItem/TaskListItem";
+import { ThemeColors, TaskColors } from "../../ThemeColors";
+import PrereqListItem from "../PrereqListItem/PrereqListItem";
 
-const ListView = () => {
-  return (
-    <div className={styles.listView}>
-      <ListViewItem />
-      <ListViewItem />
-      <ListViewItem />
-      <ListViewItem />
-      <ListViewItem />
-      <ListViewItem />
-    </div>
-  );
+const ListView = ({ tasks, TaskType }) => {
+  const convertInt = (x) => {
+    if (x.task_done && x.prereqs_done) {
+      return 0;
+    } else if (!x.task_done && !x.prereqs_done) {
+      return 1;
+    } else {
+      return 2;
+    }
+  };
+
+  tasks.sort(function (a, b) {
+    return convertInt(b) - convertInt(a);
+  });
+
+  const listItems = tasks.map(function (task, ix) {
+    const taskColor = task.task_done
+      ? TaskColors.FinishedTask
+      : task.prereqs_done
+      ? TaskColors.ValidTask
+      : TaskColors.InvalidTask;
+    return (
+      <li key={ix} className={styles.TaskItemList}>
+        <TaskType taskName={task.name} taskStatusColor={taskColor} />
+      </li>
+    );
+  });
+
+  return <div className={styles.listView}>{listItems}</div>;
+};
+
+ListView.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
+  TaskType: PropTypes.elementType.isRequired,
 };
 
 export default ListView;
