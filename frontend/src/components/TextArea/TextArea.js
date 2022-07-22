a1132b94dee8f092aa5fbdc0a61f58226933cb03
nicholaas11 <nicholas.susanto02@gmail.com>
2022-07-22 16:29:50 -0400

    Add onChange callback parameter to TextArea

--- frontend/src/components/TextArea/TextArea.js
+++ frontend/src/components/TextArea/TextArea.js
@@ -1,8 +1,12 @@
 import PropTypes from "prop-types";
 import styles from "./TextArea.module.css";
 
-const TextArea = () => {
-  return <textarea className={styles.textArea} />;
+const TextArea = ({ onChange }) => {
+  return <textarea className={styles.textArea} onChange={onChange} />;
+};
+
+TextArea.propTypes = {
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TextArea;
