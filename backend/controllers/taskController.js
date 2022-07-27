9e0b53aec047ade4f6f703e30aee6011f0277b3a
Vallens Kho <secure.mail2357@gmail.com>
2022-07-27 11:55:49 -0400

    Update create task controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,31 +1,66 @@
+const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 const Task = require("../models/taskModel");
+const { runTxWithResults } = require("../utils");
 
 const createTask = asyncHandler(async (req, res) => {
   const taskBody = {
     ...req.body,
     user_id: req.uid,
+    task_done: false,
+    dependents: [],
   };
-  const newTask = await new Task(taskBody);
 
-  await newTask
-    .save()
-    .then((result) => {
-      // 201 --> http create success status code
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      if (err.name == "ValidationError") {
-        const errorMessage = "Invalid data for a task";
-        // 400 --> server won't/can't process request due to a perceived
-        // client error
-        res.status(400).json({ message: errorMessage, server_err: err.name });
+  if (!("prereqs" in req.body)) {
+    res
+      .status(400)
+      .json({ message: "Invalid data for a task", server_err: "" });
+    return;
+  }
+
+  // Starts the session to be used for the transaction
+  const db = await mongoose.createConnection(process.env.ATLAS_URI).asPromise();
+  const session = await db.startSession();
+
+  const txRes = await runTxWithResults(session, async () => {
+    const txRes_ = {
+      status: 500,
+      body: { message: "Error creating task", server_err: ""},
+    }
+
+    const prereqDocs = await Task.find({
+      _id: { $in: req.body.prereqs },
+      user_id: req.uid,
+    }).session(session);
+
+    // compute prereqs_done
+    const prereqs_done = prereqDocs.reduce(
+      (status, doc) => status && doc.task_done,
+      true
+    );
+    taskBody.prereqs_done = prereqs_done;
+
+    try {
+      const newTask = await Task.create([taskBody], { session: session });
+      txRes_.status = 201;
+      txRes_.body = newTask[0];
+    } catch (err) {
+      if ("name" in err && err.name == "ValidationError") {
+        txRes_.status = 400;
+        txRes_.body = {message: "Invalid data for a task", server_err: err.name};
       }
 
-      const errorMessage = "Error creating task";
-      // 500 --> something went wrong server-side
-      res.status(500).json({ message: errorMessage, server_err: "" });
-    });
+      const serv_err = "name" in err ? err.name : "";
+      txRes_.status = 500;
+      txRes_.body = { message: "Error creating task", server_err: serv_err};
+    }
+
+    return txRes_;
+  });
+
+  await session.endSession();
+
+  res.status(txRes.status).json(txRes.body);
 });
 
 const getAllTasks = asyncHandler(async (req, res) => {
