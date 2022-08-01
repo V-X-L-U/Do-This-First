b5fad29abfa67c7c0151ead30441e356e9bcef00
Vallens Kho <secure.mail2357@gmail.com>
2022-08-01 15:01:20 -0400

    Implement URL for marking task done

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { markTaskDone } = require("../controllers/markTaskController");
 const { createTask, getAllTasks } = require("../controllers/taskController");
 const { authenticateUser } = require("../middlewares/authenticateUser");
 
@@ -12,5 +12,9 @@ router.post("/create", [authenticateUser, createTask]);
 // Get all tasks for a user.
 // GET /api/tasks/getAll
 router.get("/getAll", [authenticateUser, getAllTasks]);
+
+// Attempt to mark a task done
+// PUT /api/tasks/markDone/:id
+router.put("/markDone/:id", [authenticateUser, markTaskDone]);
 
 module.exports = router;
