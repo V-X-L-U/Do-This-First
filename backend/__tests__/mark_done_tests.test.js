39caa7aa11e0db132bd0bddeb38ee7b2bc4a6f75
Vallens Kho <secure.mail2357@gmail.com>
2022-08-02 17:42:13 -0400

    Set up basic test case for marking a task done

---
+++ backend/__tests__/mark_done_tests.test.js
@@ -0,0 +1,79 @@
+const request = require("supertest");
+const { app } = require("../server");
+const mongoose = require("mongoose");
+
+const Task = require("../models/taskModel");
+
+const {
+  twoTaskSetup,
+  threeTaskSetup,
+  multLinearSetup,
+  multNonLinearSetup,
+  assertGrey,
+  assertRed,
+  assertStriked,
+} = require("./mark_task_helpers");
+const {
+  loginUser,
+  logoutUser,
+  setupTestServer,
+  tearDownTestServer,
+  expectStandardResponse,
+  expectUserNotAuthenticated,
+  expectInvalidTokenErr,
+} = require("./test_helpers");
+
+const credentials = {
+  email: "mark.done@gmail.com",
+  password: "123456uU",
+};
+
+let server;
+let userId;
+let jwt;
+
+beforeAll(async () => {
+  const serverValues = await setupTestServer(credentials);
+  server = serverValues.server;
+  userId = serverValues.userId;
+});
+
+afterAll(async () => {
+  await tearDownTestServer(server, credentials);
+});
+
+describe("Task Mark Done Test Suite", () => {
+  beforeEach(async () => {
+    jwt = await loginUser(credentials);
+  });
+
+  afterEach(async () => {
+    await logoutUser(credentials);
+    await Task.deleteMany({});
+  });
+  const markDoneRoute = (taskId) => `/api/tasks/markDone/${taskId}`;
+  const markDoneCall = (taskData) => {
+    return request(app)
+      .put(markDoneRoute(taskData._id))
+      .set("cookie", jwt)
+      .send({});
+  };
+
+  it("Basic mark done", async () => {
+    const [root, dep1] = await twoTaskSetup(jwt);
+    await assertRed(root);
+    await assertGrey(dep1);
+
+    const res1 = await markDoneCall(root);
+    expectStandardResponse(res1, 200, "Successfully marked task done", "");
+
+    await assertStriked(root);
+    await assertRed(dep1);
+
+    const res2 = await markDoneCall(dep1);
+    expectStandardResponse(res2, 200, "Successfully marked task done", "");
+
+    await assertStriked(root);
+    await assertStriked(dep1);
+  });
+});
---
+++ backend/__tests__/mark_task_helpers.js
@@ -0,0 +1,120 @@
+const request = require("supertest");
+const { app } = require("../server");
+const mongoose = require("mongoose");
+
+const Task = require("../models/taskModel");
+
+// Create a task through the corresponding API route.
+// Relies on the create API being correct.
+const createTask = async (jwt, taskData) => {
+  const res = await request(app)
+    .post("/api/tasks/create")
+    .set("cookie", jwt)
+    .send(taskData);
+  expect(res.statusCode).toEqual(201);
+  return res;
+};
+
+const twoTaskSetup = async (jwt) => {
+  const root = {
+    name: "root task",
+    description: "sth",
+    prereqs: [],
+  };
+
+  const rootRes = await createTask(jwt, root);
+
+  const dep1 = {
+    name: "dep 1",
+    description: "sth",
+    prereqs: [rootRes.body._id],
+  };
+
+  const dep1Res = await createTask(jwt, dep1);
+
+  return [rootRes.body, dep1Res.body];
+};
+
+const threeTaskSetup = async (jwt) => {
+  const [root, dep1] = await twoTaskSetup(jwt);
+  const dep2 = {
+    name: "dep 2",
+    description: "sth",
+    prereqs: [root._id, dep1._id],
+  };
+
+  const dep2Res = await createTask(jwt, dep2);
+
+  return [root, dep1, dep2Res.body];
+};
+
+// All tasks depend on the root and the root only.
+const multLinearSetup = async (jwt) => {
+  const [root, dep1] = await twoTaskSetup(jwt);
+  const dep2 = {
+    name: "dep 2",
+    description: "sth",
+    prereqs: [root._id],
+  };
+
+  const dep2Res = await createTask(jwt, dep2);
+
+  const dep3 = {
+    name: "dep 3",
+    description: "sth",
+    prereqs: [root._id],
+  };
+
+  const dep3Res = await createTask(jwt, dep3);
+
+  return [root, dep1, dep2Res.body, dep3Res.body];
+};
+
+// A set up where dependents of a task have edges between each other.
+const multNonLinearSetup = async (jwt) => {
+  const [root, dep1, dep2] = await threeTaskSetup(jwt);
+
+  const dep3 = {
+    name: "dep 3",
+    description: "sth",
+    prereqs: [root._id, dep2._id],
+  };
+
+  const dep3Res = await createTask(jwt, dep3);
+
+  return [root, dep1, dep2Res.body, dep3Res.body];
+};
+
+const getTaskById = (taskData) => {
+  return Task.findOne({ _id: taskData._id, user_id: taskData.user_id });
+};
+
+const assertGrey = async (taskData) => {
+  const taskToAssert = await getTaskById(taskData);
+  expect(taskToAssert.prereqs_done).toEqual(false);
+  expect(taskToAssert.task_done).toEqual(false);
+};
+
+const assertRed = async (taskData) => {
+  const taskToAssert = await getTaskById(taskData);
+  expect(taskToAssert.prereqs_done).toEqual(true);
+  expect(taskToAssert.task_done).toEqual(false);
+};
+
+const assertStriked = async (taskData) => {
+  const taskToAssert = await getTaskById(taskData);
+  console.log(taskToAssert);
+  expect(taskToAssert.prereqs_done).toEqual(true);
+  expect(taskToAssert.task_done).toEqual(true);
+};
+
+module.exports = {
+  createTask,
+  twoTaskSetup,
+  threeTaskSetup,
+  multNonLinearSetup,
+  multLinearSetup,
+  assertGrey,
+  assertRed,
+  assertStriked,
+};
