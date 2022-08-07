98feb5b36650c0b33aa8c2f56ef8688c512dd9c9
Vallens Kho <69379333+V-X-L-U@users.noreply.github.com>
2022-08-07 17:52:03 -0400

    Mark task done (#20)
    * Fix test to ensure user is always removed after tests finish
    
    * Refactor tx helpers to a separate file
    
    * Implement URL for marking task done
    
    * Implement helper to check for valid Mongo ObjectIds
    
    * Implement mark task done controller
    
    * Fix missing import
    
    * Move mark done tx helpers to separate file
    
    * Fix imports
    
    * Fix mark task done controller to update task_done field
    
    * Set up basic test case for marking a task done
    
    * Fix mark task done controller to pass basic test case
    
    * Update mark task to inform client of invalid ObjectId
    
    * Implement TxError to help with aborting tx
    
    * Implement tx abort for markTaskDone
    
    * Fix tx error bubbling up beyond tx
    
    * Add mark done tests for task not found case
    
    * Make Jest test run sequentially
    
    * Add mark done test for prereqs not done case
    
    * Add subsequent mark done test
    
    * Add basic assertions in task set up helpers
    
    * Add update multiple prereqs test
    
    * Add mixed successful and failed mark done test
    
    * Add verify update isolated to task forest test
    
    Co-authored-by: Vallens Kho <secure.mail2357@gmail.com>

---
+++ backend/controllers/markTaskController.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
+const Task = require("../models/taskModel");
+const TxError = require("./txError");
+const { runTxWithResults, checkValidObjectIds } = require("../utils");
+const {
+  getTaskById,
+  taskDoneUpdateDirects,
+  updateTaskHelper,
+} = require("./markTaskDoneTxHelpers");
+
+const makeSuccessTxRes = (txRes) => {
+  (txRes.status = 200),
+    (txRes.body = { message: "Successfully marked task done", server_err: "" });
+};
+
+const markTaskDone = asyncHandler(async (req, res) => {
+  const markDoneId = req.params.id;
+  const userId = req.uid;
+  const txRes = {
+    status: 400,
+    body: {},
+  };
+
+  await mongoose.connection
+    .transaction(async (session) => {
+      const taskToMark = await getTaskById(session, markDoneId, userId, txRes);
+      if (!taskToMark) {
+        throw new TxError("Task not found");
+      }
+
+      if (taskToMark.task_done) {
+        makeSuccessTxRes(txRes);
+        throw new TxError("Task not done");
+      }
+
+      if (!taskToMark.prereqs_done) {
+        txRes.status = 400;
+        txRes.body = {
+          message: "Some prerequisites are not yet done",
+          server_err: "",
+        };
+        throw new TxError("Prereqs not done");
+      }
+
+      const markTaskFailed = await updateTaskHelper(
+        session,
+        markDoneId,
+        userId,
+        txRes
+      );
+      if (markTaskFailed) throw new TxError("Failed to mark task doc as done");
+
+      const updateDirectsFailed = await taskDoneUpdateDirects(
+        session,
+        taskToMark.dependents,
+        userId,
+        txRes
+      );
+      if (updateDirectsFailed) throw new TxError("Failed to update dependents");
+
+      makeSuccessTxRes(txRes);
+    })
+    .catch((err) => {
+      if (err.name !== "TxError") {
+        txRes.status = 500;
+        txRes.body = {
+          message: "Error marking task done",
+          server_err: `[tx] ${err.toString()}`,
+        };
+      }
+    });
+
+  res.status(txRes.status).json(txRes.body);
+  await session.endSession();
+});
+
+module.exports = { markTaskDone };
---
+++ backend/controllers/markTaskDoneTxHelpers.js
@@ -0,0 +1,118 @@
+const Task = require("../models/taskModel");
+const { checkValidObjectIds } = require("../utils");
+
+// Attempts to get a user task by it's id.
+// Returns null iff. one of the following errors occurred:
+//  - invalid task id
+//  - task doesn't exist
+//  - task doesn't belong to the user
+//  - some unexpected error from anywhere in the operation
+// Modifies <txRes_> to error responses as needed.
+const getTaskById = async (session, taskId, userId, txRes_) => {
+  try {
+    if (!checkValidObjectIds([taskId])) {
+      txRes_.status = 400;
+      txRes_.body = {
+        message: "Task does not exist",
+        server_err: "Invalid ObjectId",
+      };
+      return;
+    }
+
+    const taskDoc = await Task.findOne({
+      _id: taskId,
+      user_id: userId,
+    }).session(session);
+    if (!taskDoc) {
+      txRes_.status = 400;
+      txRes_.body = {
+        message: "Task does not exist",
+        server_err: "",
+      };
+      return;
+    }
+
+    return taskDoc;
+  } catch (err) {
+    txRes_.status = 500;
+    txRes_.body = {
+      message: "Error marking task done",
+      server_err: `[get task by id] ${err.toString()}`,
+    };
+
+    return;
+  }
+};
+
+// Updates the direct dependents of a task that is marked done.
+// Returns <True> iff. an unexpected error occurred.
+// Modifies <txRes_> to indicate the error thrown.
+const taskDoneUpdateDirects = async (session, dependents, userId, txRes_) => {
+  try {
+    // find all dependents
+    const depTasks = await Task.find({
+      _id: { $in: dependents },
+      user_id: userId,
+    }).session(session);
+
+    // update each dependent
+    const updateDepPromises = depTasks.map(async (taskDoc) => {
+      // eval prereq status
+      const prereqDocs = await Task.find({
+        _id: { $in: taskDoc.prereqs },
+        user_id: userId,
+      }).session(session);
+      const prereqsDone = prereqDocs.reduce(
+        (status, t) => status && t.task_done,
+        true
+      );
+
+      // update the prereq status
+      await Task.findOneAndUpdate(
+        { _id: taskDoc._id, user_id: taskDoc.user_id },
+        { prereqs_done: prereqsDone }
+      ).session(session);
+    });
+
+    await Promise.all(updateDepPromises);
+
+    return false;
+  } catch (err) {
+    txRes_.status = 500;
+    txRes_.body = {
+      message: "Error marking task done",
+      server_err: `[task done update] ${err.toString()}`,
+    };
+
+    return true;
+  }
+};
+
+// Updates the task to mark it as done
+// Returns <True> iff. an unexpected error occurred.
+// Modifies <txRes_> to indicate the appropriate error response.
+const updateTaskHelper = async (session, taskId, userId, txRes_) => {
+  return Task.findOneAndUpdate(
+    { _id: taskId, user_id: userId },
+    { task_done: true }
+  )
+    .session(session)
+    .then(() => {
+      return false;
+    })
+    .catch((err) => {
+      txRes_.status = 500;
+      txRes_.body = {
+        message: "Error marking task done",
+        server_err: `[mark task done] ${err.toString()}`,
+      };
+
+      return true;
+    });
+};
+
+module.exports = {
+  updateTaskHelper,
+  getTaskById,
+  taskDoneUpdateDirects,
+};
