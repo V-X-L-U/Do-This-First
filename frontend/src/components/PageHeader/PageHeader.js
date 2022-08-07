92244786374f6b2acaa28e87a043b64b735a2f0c
Vallens Kho <secure.mail2357@gmail.com>
2022-08-07 19:16:40 -0400

    Implement NavButton

--- frontend/src/components/PageHeader/PageHeader.js
+++ frontend/src/components/PageHeader/PageHeader.js
@@ -1,7 +1,23 @@
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
 import styles from "./PageHeader.module.css";
 import PropTypes from "prop-types";
 
-const PageHeader = ({ pageTitle, bgColor, trailingComp }) => {
+const NavButton = ({ isOpen, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {!isOpen ? (
+        <FiMenu className={styles.hamburgerIcon} />
+      ) : (
+        <MdClose className={styles.closeIcon} />
+      )}
+    </button>
+  );
+};
+
+const PageHeader = ({ pageTitle, bgColor, trailingComp, showNav }) => {
+  const [showNavDropdown, setShowNavDropdown] = useState(false);
   return (
     <h1
       className={styles.pageHeader}
@@ -9,16 +25,23 @@ const PageHeader = ({ pageTitle, bgColor, trailingComp }) => {
         backgroundColor: bgColor,
       }}
     >
+      {showNav ? <NavButton isOpen={false} /> : <></>}
       {pageTitle}
       {trailingComp === null ? <></> : trailingComp}
     </h1>
   );
 };
 
+NavButton.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 PageHeader.propTypes = {
   pageTitle: PropTypes.string.isRequired,
   bgColor: PropTypes.string.isRequired,
   trailingComp: PropTypes.element,
+  showNav: PropTypes.bool,
 };
 
 export default PageHeader;
