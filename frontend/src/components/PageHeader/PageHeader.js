478539b9dbf5371a16c7b28140946ee0386154e1
Vallens Kho <secure.mail2357@gmail.com>
2022-08-07 20:05:16 -0400

    Implement PageHeader navigation dropdown

--- frontend/src/components/PageHeader/PageHeader.js
+++ frontend/src/components/PageHeader/PageHeader.js
@@ -1,13 +1,42 @@
+import { logoutUserHandler } from "./PageHeaderLogic";
+import { useNavigate } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { useState } from "react";
 import styles from "./PageHeader.module.css";
 import PropTypes from "prop-types";
 
-const NavButton = ({ isOpen, onClick }) => {
+const SidebarItem = ({ label, action }) => {
   return (
-    <button onClick={onClick}>
-      {!isOpen ? (
+    <button className={styles.navDropdownItem} onClick={action}>
+      {label}
+    </button>
+  );
+};
+
+const Sidebar = ({ onClose }) => {
+  const navigate = useNavigate();
+  const logUserOut = async () => {
+    const errMessage = await logoutUserHandler();
+    if (errMessage.length === 0) navigate("/");
+  };
+
+  return (
+    <div className={styles.navDropdown}>
+      <NavButton isShowingDropdown={true} onClick={onClose} />
+      <div className={styles.menuItemContainer}>
+        <SidebarItem label={"TODO"} action={() => navigate("/tasks")} />
+        <SidebarItem label={"Create Task"} action={() => navigate("/create")} />
+        <SidebarItem label={"Log out"} action={logUserOut} />
+      </div>
+    </div>
+  );
+};
+
+const NavButton = ({ isShowingDropdown, onClick }) => {
+  return (
+    <button className={styles.navButton} onClick={onClick}>
+      {!isShowingDropdown ? (
         <FiMenu className={styles.hamburgerIcon} />
       ) : (
         <MdClose className={styles.closeIcon} />
@@ -25,15 +54,24 @@ const PageHeader = ({ pageTitle, bgColor, trailingComp, showNav }) => {
         backgroundColor: bgColor,
       }}
     >
-      {showNav ? <NavButton isOpen={false} /> : <></>}
+      {showNav ? <NavButton isShowingDropdown={false} /> : <></>}
       {pageTitle}
       {trailingComp === null ? <></> : trailingComp}
     </h1>
   );
 };
 
+SidebarItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+};
+
+Sidebar.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
 NavButton.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
+  isShowingDropdown: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
