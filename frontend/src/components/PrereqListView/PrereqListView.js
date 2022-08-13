5d9561f07b5a2a63e4bae8f455b3152237c2890f
nicholaas11 <nicholas.susanto02@gmail.com>
2022-08-13 05:35:05 -0400

    Add PrereqListView component

---
+++ frontend/src/components/PrereqListView/PrereqListView.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import styles from "./PrereqListView.module.css";
+import PropTypes from "prop-types";
+import { ThemeColors, TaskColors } from "../../ThemeColors";
+import AddListItem from "../AddListItem/AddListItem";
+
+const PrereqListView = ({ userTasks, prereqTask }) => {
+  const convertTaskToInt = (taskToConvert) => {
+    if (taskToConvert.task_done && taskToConvert.prereqs_done) {
+      return 0;
+    } else if (!taskToConvert.task_done && !taskToConvert.prereqs_done) {
+      return 1;
+    } else {
+      return 2;
+    }
+  };
+
+  userTasks.sort(function (task1, task2) {
+    return convertTaskToInt(task2) - convertTaskToInt(task1);
+  });
+
+  const prereqStrings = [];
+
+  for (const i in prereqTask) {
+    prereqStrings.push(prereqTask[i]._id);
+  }
+
+  const listItems = userTasks.map(function (task, ix) {
+    const taskColor = task.task_done
+      ? TaskColors.Finished
+      : task.prereqs_done
+      ? TaskColors.Valid
+      : TaskColors.Invalid;
+
+    const containsPrereq = () => {
+      return prereqStrings.includes(task._id);
+    };
+
+    return (
+      <li key={ix} className={styles.listItem}>
+        <AddListItem
+          taskName={task.name}
+          taskStatusColor={taskColor}
+          check={containsPrereq()}
+          prereqTask={prereqTask}
+        />
+      </li>
+    );
+  });
+
+  return <div className={styles.prereqListView}>{listItems}</div>;
+};
+
+PrereqListView.propTypes = {
+  userTasks: PropTypes.arrayOf(PropTypes.object).isRequired,
+  prereqTask: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default PrereqListView;
