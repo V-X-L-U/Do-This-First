c4e312946d73ecd0fcb15092447dae38bc9ba4be
Vallens Kho <secure.mail2357@gmail.com>
2022-09-03 13:50:08 -0400

    Remove delete button from PrereqListItem

--- frontend/src/components/PrereqListItem/PrereqListItem.js
+++ frontend/src/components/PrereqListItem/PrereqListItem.js
@@ -1,11 +1,8 @@
 import PropTypes from "prop-types";
-import { TiDelete } from "react-icons/ti";
 import styles from "./PrereqListItem.module.css";
 import { TaskColors } from "../../ThemeColors";
 
 const PrereqListItem = ({ taskName, taskStatusColor }) => {
-  const deleteTask = () => {};
-
   return (
     <div className={styles.prereqListItem}>
       <div
@@ -17,11 +14,6 @@ const PrereqListItem = ({ taskName, taskStatusColor }) => {
         }}
       >
         {taskName}
-      </div>
-      <div className={styles.deleteBtnLayout}>
-        <button className={styles.deleteButton} onClick={deleteTask}>
-          <TiDelete className={styles.deleteIcon} />
-        </button>
       </div>
     </div>
   );
